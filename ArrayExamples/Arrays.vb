c6f36ba482ce16616fe96a13debb47183844cb22
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-10 08:26:52 -0700

    set up

---
+++ ArrayExamples/Arrays.vb
@@ -0,0 +1,25 @@
+﻿'Payden Hoskins
+'Spring 2025
+'RCET 2265
+'Array Examples
+'https://github.com/PaydenHoskins/ArrayExamples.git
+
+Option Explicit On
+Option Strict On
+
+Module Arrays
+
+    Sub Main()
+        'arrays are cool
+        Dim Fruits(5) As String
+
+        Fruits(9) = "bannana"
+
+        Fruits(3) = "grape"
+
+        'Fruits(6) = "apple"
+
+        Console.WriteLine()
+    End Sub
+
+End Module

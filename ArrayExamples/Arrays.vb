93ea9f722ccf8a478706f88acaa744ff5e9efed4
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-10 09:01:16 -0700

    Added students

--- ArrayExamples/Arrays.vb
+++ ArrayExamples/Arrays.vb
@@ -10,6 +10,13 @@ Option Strict On
 Module Arrays
 
     Sub Main()
+
+        MultiDimentionalArrays()
+
+
+    End Sub
+
+    Sub SimpleArray()
         'arrays are cool
         Dim Fruits(5) As String
         Dim Names() As String = {"john", "Paul", "ringo", "jeff"}
@@ -32,19 +39,18 @@ Module Arrays
         Next
 
 
-
-
-
-    End Sub
-
-    Sub SimpleArray()
-
     End Sub
 
     Sub MultiDimentionalArrays()
         Dim Students(14, 2) As Single
 
+        Students(0, 0) = 34
+        Students(0, 1) = 67
+        Students(0, 2) = 94
 
+        Console.WriteLine(Students(0, 0))
+        Console.WriteLine(Students(0, 1))
+        Console.WriteLine(Students(0, 2))
     End Sub
 
 
